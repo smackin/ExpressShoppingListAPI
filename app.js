9f883e3739a3ac9050f6e35f587289f5a73d13b0
Stephanie <smmackin12@gmail.com>
2022-08-03 13:04:49 -0400

    router working

--- app.js
+++ app.js
@@ -1,46 +1,26 @@
-const express = require('express');
-const ExpressError = require("./expressError")
-const items = require("./fakeDb")
-
+const express = require("express")
 const app = express();
+const itemsRoutes = require("./routes/items")
+const ExpressError = require("./expressError")
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/items", itemsRoutes);
 
-app.get('/items', function(req,res) {
-// render a list of shopping items as an array of objects [{“name”: “popsicle”, “price”: 1.45}, {“name”:”cheerios”, “price”: 3.40}]
-    return res.send("/items route!")
-    // res.json({items})
-})
+/** 404 handler */
 
-app.post ('/items' , function (req, res, next) {
-// accept JSON data and push it to the shopping list. 
+app.use(function (req, res, next) {
+    return new ExpressError("Not Found", 404);
+});
 
-})
+/** general error handler */
 
-app.get ('/items/:name', function(req,res){
-// display a single item's name and price 
+app.use((err, req, res, next) => {
+res.status(err.status || 500);
 
-    return res.send()
-})
+    return res.json({
+    error: err.message,
+});
+});
 
-
-app.patch('/items/:name', function (req, res) { 
-//modify a single items name and/or price 
-
-})
-
-
-
-
-app.delete('/items/:name', function (req, res) {
-//delete item from a array based on :name
-
-})
-
-
-
-
-
-app.listen(3000, function () {
-    console.log('App on port 3000');
-})
+module.exports = app
